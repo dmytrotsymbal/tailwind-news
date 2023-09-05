6c7ce0adf6a37b32172b8eaae87195ec73bf50b8
Dima <dima.tsymbal@outlook.com>
2023-09-05 12:55:31 +0300

    fetching and styles processing

--- src/script.js
+++ src/script.js
@@ -1,58 +1,41 @@
-const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+const apiKey = "9e9b7d1c2042476586e5d3584b6e7cab";
+const apiUrl = `https://newsapi.org/v2/top-headlines?country=ua&apiKey=${apiKey}`;
 
-// Создаем функцию для выполнения запроса к API
-async function fetchPosts() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// Асинхронна функція для отримання та відображення новин
+async function fetchNews() {
+  const res = await fetch(apiUrl);
   const data = await res.json();
 
-  // Получаем массив постов из ответа
-  const posts = data;
+  const news = data.articles;
+  const newsSection = document.getElementById("newsSection");
+  newsSection.innerHTML = "";
 
-  // Находим наш секшн с постами
-  const section = document.getElementById("newsSection");
+  // Використовуємо HTML-розмітку без окремої функції
+  news.forEach((article) => {
+    newsSection.innerHTML += `
+      <a href="${article.url}" target="_blank" class="block mx-2">
+          <div class="newsCard">
+            <div class="left">
+              <img class="cardImg" src="${article.urlToImage}" alt="News Image">
+            </div>
 
-  // Создаем цикл для перебора постов и создания HTML-разметки
-  posts.slice(0, 6).forEach((post) => {
-    // Создаем элементы HTML для каждого поста
-    const link = document.createElement("a");
-    link.classList.add("block", "mx-2");
-    link.href = "#";
-
-    const flexDiv = document.createElement("div");
-    flexDiv.classList.add("flex");
-
-    const contentDiv = document.createElement("div");
-
-    const title = document.createElement("h2");
-    title.classList.add(
-      "dark:text-slate-200",
-      "text-3xl",
-      "mt-3",
-      "mb-1",
-      "text-gray-700",
-      "font-bold",
-      "capitalize"
-    );
-    title.textContent = post.title;
-
-    const description = document.createElement("p");
-    description.classList.add("dark:text-slate-400", "text-gray-700");
-    description.textContent = post.body;
-
-    // Собираем элементы вместе
-    contentDiv.appendChild(title);
-    contentDiv.appendChild(description);
-
-    flexDiv.appendChild(contentDiv);
-
-    link.appendChild(flexDiv);
-
-    section.appendChild(link);
+            <div class="right">
+              <h2 class="cardTitle">
+              ${article.title}
+              </h2>
+              <p class="cardParagraph">${article.description}</p>
+              <p class="cardDate">${new Date(
+                article.publishedAt
+              ).toLocaleString()}</p>
+            </div>
+          </div>
+      </a>
+    `;
   });
 }
 
-// Вызываем функцию для получения и вывода постов
-fetchPosts();
+// Викликаємо функцію для отримання і відображення новин
+fetchNews();
 
 //--------------------------------------------------------------------------
 
